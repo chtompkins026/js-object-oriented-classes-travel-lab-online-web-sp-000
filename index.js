dea355152b5ec190c0cd70ebcc65861c2fcf7517
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-25 19:33:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,8 +41,10 @@ class Route {
   estimatedTime(peak){ 
     if (peak){ 
       return this.blocksTravelled() / 2; 
+    } else { 
+      return this.blocksTravelled() / 3; 
+      
     }
-    
   }
   
   
